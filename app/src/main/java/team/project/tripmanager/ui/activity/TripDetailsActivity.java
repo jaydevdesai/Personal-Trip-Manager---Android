127d3aa54a24c52de35e5e66a0d5243eb753e2f0
Admin <jaydevdesai15@gmail.com>
2019-03-19 22:32:49 +0530

    Update Trip Fragment created.

--- app/src/main/java/team/project/tripmanager/ui/activity/TripDetailsActivity.java
+++ app/src/main/java/team/project/tripmanager/ui/activity/TripDetailsActivity.java
@@ -1,8 +1,10 @@
 package team.project.tripmanager.ui.activity;
 
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageButton;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.CardView;
@@ -17,14 +19,18 @@ import android.view.ViewGroup;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 import team.project.tripmanager.R;
 import team.project.tripmanager.model.Trip;
+import team.project.tripmanager.ui.fragment.CreateTripFragment;
+import team.project.tripmanager.ui.fragment.EditTripFragment;
 import team.project.tripmanager.utils.DateUtils;
 
 public class TripDetailsActivity extends AppCompatActivity {
 
     AppCompatTextView tripTitleTv, tripDatesTv, placeNameTv;
+    AppCompatImageButton editTripBtn;
     RecyclerView cardsListView;
     ArrayList<String> cardsList;
     ArrayList<Integer> cardsListImage;
@@ -38,6 +44,7 @@ public class TripDetailsActivity extends AppCompatActivity {
         tripTitleTv = findViewById(R.id.tripTitle);
         tripDatesTv = findViewById(R.id.tripDates);
         placeNameTv = findViewById(R.id.placeName);
+        editTripBtn = findViewById(R.id.editTripBtn);
         cardsListView = findViewById(R.id.cardsList);
 
         cardsList = new ArrayList<String>(Arrays.asList("Group", "Reservations", "Documents","Expenses", "Notes", "Shopping-List"));
@@ -56,6 +63,24 @@ public class TripDetailsActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         placeNameTv.setText("Visiting "+tripDetails.getPlaceName()+".");
+
+        editTripBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditTripFragment editTripFragment = EditTripFragment.newInstance();
+                editTripFragment.setCurrentTrip(tripDetails);
+                editTripFragment.setCancelable(false);
+                editTripFragment.show(getSupportFragmentManager(), "editTrip");
+                getSupportFragmentManager().executePendingTransactions();
+                editTripFragment.getDialog().setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        getSupportFragmentManager().beginTransaction().remove(editTripFragment).commit();
+
+                    }
+                });
+            }
+        });
     }
 
     public class CardsListHolder extends RecyclerView.Adapter<TripDetailsActivity.CardsListHolder.MyViewHolder> {
--- app/src/main/java/team/project/tripmanager/ui/fragment/EditTripFragment.java
+++ app/src/main/java/team/project/tripmanager/ui/fragment/EditTripFragment.java
@@ -1,7 +1,6 @@
 package team.project.tripmanager.ui.fragment;
 
 import android.app.DatePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,63 +12,60 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
-import android.widget.Toast;
 
-import com.google.android.gms.common.api.Status;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.widget.Autocomplete;
-import com.google.android.libraries.places.widget.AutocompleteActivity;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.Objects;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import team.project.tripmanager.R;
-import team.project.tripmanager.core.TMApplication;
-import team.project.tripmanager.core.TMEnvironment;
 import team.project.tripmanager.logger.Logger;
-import team.project.tripmanager.model.CommonResponse;
-import team.project.tripmanager.model.ErrorResponse;
+import team.project.tripmanager.model.Trip;
+import team.project.tripmanager.utils.DateUtils;
 
-import static android.app.Activity.RESULT_CANCELED;
+public class EditTripFragment extends DialogFragment {
 
-public class CreateTripFragment extends DialogFragment {
-
-    int AUTOCOMPLETE_REQUEST_CODE = 1, FLAG = 0, BtnFlag = 0;
+    int FLAG = 0, BtnFlag = 0;
     String startDateString, endDateString;
     AppCompatEditText tripNameEdt, startDateEdt, endDateEdt, selectPlaceEdt;
     Calendar myCalendar;
     DatePickerDialog.OnDateSetListener onDateSetListener;
-    AppCompatTextView createBtn, cancelBtn;
+    AppCompatTextView updateBtn, cancelBtn, deleteBtn;
     TextInputLayout tripNameInput, selectPlaceInput;
+    Trip currentTrip;
     private Logger logger = new Logger(getClass());
 
-    public static CreateTripFragment newInstance(){
-        return new CreateTripFragment();
+    public static EditTripFragment newInstance(){ return new EditTripFragment();}
+
+    public void setCurrentTrip(Trip currentTrip) {
+        this.currentTrip = currentTrip;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View v = inflater.inflate(R.layout.fragment_create_trip, container, false);
+        View v = inflater.inflate(R.layout.fragment_edit_trip, container, false);
 
         tripNameEdt = v.findViewById(R.id.tripNameEdt);
         selectPlaceEdt = v.findViewById(R.id.selectPlaceEdt);
         startDateEdt = v.findViewById(R.id.startDateEdt);
         endDateEdt = v.findViewById(R.id.endDateEdt);
-        createBtn = v.findViewById(R.id.createBtn);
+        updateBtn = v.findViewById(R.id.updateBtn);
         cancelBtn = v.findViewById(R.id.cancelBtn);
+        deleteBtn = v.findViewById(R.id.deleteBtn);
         tripNameInput = v.findViewById(R.id.tripNameInput);
         selectPlaceInput = v.findViewById(R.id.selectPlaceInput);
         myCalendar = Calendar.getInstance();
+
+        tripNameEdt.setText(currentTrip.getTripName());
+        selectPlaceEdt.setText(currentTrip.getPlaceName());
+        try {
+            startDateEdt.setText(DateUtils.getFormattedDate(currentTrip.getStartDate(),"dd MMM yy"));
+            endDateEdt.setText(DateUtils.getFormattedDate(currentTrip.getEndDate(),"dd MMM yy"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         onDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -99,21 +95,20 @@ public class CreateTripFragment extends DialogFragment {
                 FLAG = 2;
             }
         });
-
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getDialog().dismiss();
             }
         });
-        createBtn.setOnClickListener(new View.OnClickListener() {
+        updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tripNameEdt.getText().length() !=0 && selectPlaceEdt.getText().length() !=0){
-                    createTripRequestToServer(tripNameEdt.getText().toString(),
+                if(Objects.requireNonNull(tripNameEdt.getText()).length() !=0 && Objects.requireNonNull(selectPlaceEdt.getText()).length() !=0){
+                    /*updateTripRequestToServer(tripNameEdt.getText().toString(),
                             selectPlaceEdt.getText().toString(),
                             startDateString,
-                            endDateString);
+                            endDateString);*/
                 } else {
                     tripNameInput.setError("Field Required");
                     selectPlaceInput.setError("Field Required");
@@ -123,35 +118,7 @@ public class CreateTripFragment extends DialogFragment {
         return v;
     }
 
-    private void createTripRequestToServer(String tripName, String placeName, String startDate, String endDate) {
-        TMEnvironment environment = ((TMApplication) getActivity().getApplicationContext()).getEnvironment();
-        environment.getAPIService().createTrip(tripName, placeName, startDate, endDate).enqueue(new Callback<CommonResponse>() {
-            @Override
-            public void onResponse(Call<CommonResponse> call, Response<CommonResponse> response) {
-                if (response.isSuccessful()) {
-                    Toast.makeText(getActivity(), response.body().getMessage(), Toast.LENGTH_SHORT).show();
-                    getDialog().dismiss();
-                } else if(response.errorBody() != null){
-                    try {
-                        Gson gson = new GsonBuilder().setLenient().create();
-                        ErrorResponse errorResponse = gson.fromJson(response.errorBody().string(), ErrorResponse.class);
-                        Toast.makeText(getActivity(), errorResponse.getMessage(), Toast.LENGTH_SHORT).show();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    logger.debug("Unsuccessful response");
-                    Toast.makeText(getActivity(), "Something went wrong", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<CommonResponse> call, Throwable t) {
-                logger.error(t);
-                Toast.makeText(getActivity(), "Something went wrong", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+    private void updateTripRequestToServer(String tripName, String placeName, String startDate, String endDate){}
 
     private void showDate() {
 
@@ -167,27 +134,10 @@ public class CreateTripFragment extends DialogFragment {
         switch (FLAG){
             case 1 : startDateString = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(myCalendar.getTime());
                 startDateEdt.setText(new SimpleDateFormat("dd MMM yy", Locale.getDefault()).format(myCalendar.getTime()));
-                        break;
+                break;
             case 2 : endDateString = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(myCalendar.getTime());
                 endDateEdt.setText(new SimpleDateFormat("dd MMM yy", Locale.getDefault()).format(myCalendar.getTime()));
-                        break;
+                break;
         }
     }
-
-    /*@Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == AUTOCOMPLETE_REQUEST_CODE && data != null){
-            try{
-                Place place = Autocomplete.getPlaceFromIntent(data);
-                Toast.makeText(getActivity(),place.getName(), Toast.LENGTH_LONG).show();
-            } catch (IllegalArgumentException e){}
-        } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
-
-            Status status = Autocomplete.getStatusFromIntent(data);
-        } else if (resultCode == RESULT_CANCELED) {
-            // The user canceled the operation.
-        }
-    }*/
-
-
 }
